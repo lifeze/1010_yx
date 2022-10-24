2cd9c0eba026a3edb6ba5669094b66784b074b7c
lifeze <yangzekun@circleyo.com>
2022-10-24 20:11:47 +0800

    fix: 渲染页面日期组件样式

--- src/components/exceleditor/components/preview/grid/GridCell.vue
+++ src/components/exceleditor/components/preview/grid/GridCell.vue
@@ -56,6 +56,8 @@
         >{{value}}</el-button>
         <el-date-picker
             v-if="cellType == 'datetime'"
+            :ref="cellType"
+            :style="styleCpn(cell.style.css)"
             v-model="value"
             type="datetime"
             @change="handleChangeDate"
@@ -207,12 +209,15 @@ export default {
             if (!!css.backgroundColor) {
                 Object.assign(temp, { backgroundColor: css.backgroundColor });
             }
-            if (this.cellType == 'text' || this.cellType == 'password') {
+            if (this.cellType == 'text' || this.cellType == 'password' || this.cellType == 'datetime') {
                 this.$nextTick(() => {
                     this.$refs[this.cellType].$el.children[0].style.color = css.color || '';
                     this.$refs[this.cellType].$el.children[0].style.fontWeight = css.fontWeight || '';
                     this.$refs[this.cellType].$el.children[0].style.fontStyle = css.fontStyle || '';
                     this.$refs[this.cellType].$el.children[0].style.fontFamily = css.fontFamily || '';
+                    if (this.cellType == 'datetime') {
+                        this.$refs[this.cellType].$el.children[0].style.textDecoration = !!css.textDel ? 'line-through' : '';
+                    }
                 })
             }
             if (this.cellType == 'number') {
